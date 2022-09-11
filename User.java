ff2c0500fbb99f9f0815eecb9b64758c28d64c4c
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-09-11 17:03:35 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,13 @@
+/*
+* class - wrapper
+* class - grouping of similar elements
+* class - blueprint
+*/
+class User {
+
+    public static void main(String args[]) {
+		System.out.println("Hello World...");
+		System.out.println(args[0] + "_" + args[1]);
+		System.out.println("Program Exit...");
+    }
+}
